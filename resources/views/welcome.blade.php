1e73ffba426051ca1dd75f8a17e7d90ef69c9208
toyful <toyfulidc@yahoo.co.jp>
2020-09-29 13:52:27 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+
+    @if(Auth::check())
+        {{ Auth::user()->name }}
+    @else
+
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
             {!! Link_to_route('signup.get','Sign up now!',[],['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
+    @endif
 @endsection

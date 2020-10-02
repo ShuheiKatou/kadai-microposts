0412b2032d19477ee90ae16585c8d92f03a85e82
toyful <toyfulidc@yahoo.co.jp>
2020-10-02 14:30:34 +0000

    post

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,23 @@
 @section('content')
 
     @if(Auth::check())
-        {{ Auth::user()->name }}
+        <div clas="row">
+            <aside class="col-sm-4">
+                <div clas="card">
+                    <div class="card-header">
+                        <h3 class ="card-title">{{ Auth::user()->name }}</h3>
+                    </div>
+                    <div class="card-body">
+                        <img class="rounded img-fluid" src="{{ Gravatar::get(Auth::user()->email,['size' =>500]) }}" alt="">
+                    </div>
+                </div>
+            </aside>
+            <div class="col-sm-8">
+                @include('microposts.form')
+                @include('microposts.microposts')
+            </div>
+        </div>
     @else
-
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
